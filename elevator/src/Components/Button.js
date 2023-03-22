c54281db2e584a4a508f9fb517ec2d730e997536
Yuval Waitzman <yuvalwaitzman888@gmail.com>
2023-03-22 07:55:40 +0200

    fixing the buttons visibility bug

--- elevator/src/Components/Button.js
+++ elevator/src/Components/Button.js
@@ -1,26 +1,25 @@
 import styled from "styled-components";
 
 const CallButton = styled.button`
-  /* background-color: ${(props) => props.color};
-opacity: ${(props) =>
-    props.filter === props.color || !props.filter ? "100%" : "55%"}; */
   background-color: green;
-  border-radius: 3px;
+  border-radius: 8px;
   color: white;
+  margin-right: 4px;
+  margin-left: 8px;
+  margin-top: 15px;
   cursor: pointer;
-  /* display: inline-block; */
   font-family: CerebriSans-Regular, -apple-system, system-ui, Roboto, sans-serif;
-  display: flex;
+  display: block;
   align-items: center;
   flex-direction: row;
   text-align: center;
-  text-decoration: none;
   transition: all 250ms;
   border: 0;
   font-size: 12px;
   user-select: none;
   -webkit-user-select: none;
   touch-action: manipulation;
+  width: 3rem;
   height: 2rem;
   :hover {
     transform: scale(1.03);

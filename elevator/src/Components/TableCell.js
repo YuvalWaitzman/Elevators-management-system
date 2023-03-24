a20d7fac43612a6ee333baad6a4515053bb41e63
Yuval Waitzman <yuvalwaitzman888@gmail.com>
2023-03-24 17:32:55 +0300

    Timer from the time elevator was chosen

--- elevator/src/Components/TableCell.js
+++ elevator/src/Components/TableCell.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import ElevatorImg from "./ElevatorImg";
 import { useSelector } from "react-redux";
+import Timer from "./Timer";
 
 const ImgContainer = styled.div`
   text-align: center;
@@ -14,11 +15,26 @@ const StyledTableCell = styled.td`
 
 const TableCell = function (props) {
   const elevators = useSelector((state) => state.elevators);
-  const elevator = elevators[props.id[1] - 1];
+  const buttons = useSelector((state) => state.buttons);
+
+  //GET THE CURRENT ELEVATOR WITH SLICE (TO ENABLE 2+ DIGIT NUMBER OF ELEVATORS)
+
+  //cell 23
+  const elevator = elevators[props.id.slice(1) - 1];
+  const checkIfRenderElevator =
+    elevator.currentFloor + props.id.slice(1) === props.id;
+  console.log(
+    elevators[props.id.slice(1) - 1].destinationFloor,
+    props.id.slice(1)
+  );
+  const checkIfRenderTimer =
+    buttons[props.id[0]].status === "Waiting" &&
+    elevators[props.id.slice(1) - 1].destinationFloor === props.id[0];
 
   return (
     <StyledTableCell id={props.id}>
-      {props.isLastRow && (
+      {checkIfRenderTimer && <Timer />}
+      {checkIfRenderElevator && (
         <ImgContainer>
           <ElevatorImg
             position={elevator.currentFloor}

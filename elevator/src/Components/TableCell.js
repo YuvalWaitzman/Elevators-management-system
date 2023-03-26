90991b84a9697fd93ca041bdca99d61317cd3124
Yuval Waitzman <yuvalwaitzman888@gmail.com>
2023-03-26 19:37:29 +0300

    Changing logic - pressing buttons

--- elevator/src/Components/TableCell.js
+++ elevator/src/Components/TableCell.js
@@ -19,25 +19,26 @@ const StyledTableCell = styled.td`
 const TableCell = function (props) {
   const elevators = useSelector((state) => state.elevators);
 
+  //Elevator relevant to this table cell
   const elevator = elevators[props.elevator - 1];
-  //WRITE EXPLANATION
-  const checkIfRenderElevator =
-    elevator.currentFloor + props.elevator == props.id;
 
-  // console.log(elevators[props.elevator - 1].destinationFloor, props.floor);
+  const checkIfRenderElevator =
+    String(elevator.currentFloor) + String(props.elevator) === props.id;
+
   const checkIfRenderTimer =
     elevators[props.elevator - 1].status === "active" &&
-    Number(elevators[props.elevator - 1].destinationFloor) ===
+    Number(elevators[props.elevator - 1].destinationFloor) ==
       Number(props.floor) &&
     elevators[props.elevator - 1].currentFloor !== props.floor;
 
   //Moving images logic here
 
   const isMoving = elevator.status === "active";
+  const sameFloor =
+    Number(elevators[props.elevator - 1].currentFloor) === Number(props.floor);
 
   // elevator.destinationFloor &&
   // elevator.currentFloor !== elevator.destinationFloor;
-
   const difference = () => {
     return (elevator.destinationFloor - elevator.currentFloor) * 118;
   };
@@ -49,14 +50,14 @@ const TableCell = function (props) {
       id={props.id}
       height={props.height}
     >
-      {isMoving && checkIfRenderTimer && <Timer />}
+      {!sameFloor && isMoving && checkIfRenderTimer && <Timer />}
       {checkIfRenderElevator && (
         <ImgContainer>
           <AnimatedElevatorImg
             difference={difference()}
             moving={isMoving}
             destination={elevator.destinationFloor}
-            position={elevator.currentFloor}
+            currentFloor={elevator.currentFloor}
             color={
               elevator.status === "available"
                 ? "black"
